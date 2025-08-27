c50d3d6b76b392af25ce7dc4a7204143e525433f
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2025-08-27 13:01:49 +0330

    add reservation page and modified panelheader

--- Panel/PanelHeader.php
+++ Panel/PanelHeader.php
@@ -31,6 +31,7 @@
     <!-- هدر -->
     <header class="p-4 bg-primary text-white flex justify-between items-center shadow-md">
         <!-- دکمه منوی موبایل -->
+
         <div class="mobile-menu-button p-2 cursor-pointer rounded-full bg-secondary lg:hidden">
             <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -52,7 +53,7 @@
                 <svg class="w-4 h-4 transition-transform" id="dropdown-arrow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                 </svg>
-                <div class="dropdown-content absolute bg-white flex-col shadow-md text-black rounded-md overflow-hidden top-10 left-0 text-sm hidden" id="dropdown-content">
+                <div class="z-20 dropdown-content absolute bg-white flex-col shadow-md text-black rounded-md overflow-hidden top-10 left-0 text-sm hidden" id="dropdown-content">
                     <div class="dropdown-item logout p-3 transition-all hover:bg-red-50 text-red-600 flex items-center gap-2" id="logout-btn">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
@@ -88,7 +89,7 @@
                 </svg>
                 <p>مدیریت دستگاه ها</p>
             </a>
-            <a href="" class="menu-item flex items-center gap-2 py-4 pr-2 font-light hover:bg-primary transition-colors duration-300">
+            <a href=<?= home_url('/reservation/') ?> class="menu-item flex items-center gap-2 py-4 pr-2 font-light hover:bg-primary transition-colors duration-300">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
                 </svg>
